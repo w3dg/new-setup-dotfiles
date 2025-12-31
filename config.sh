7a3f74f47a8b2472252c824094f973f6988edcd3
w3dg <51162936+w3dg@users.noreply.github.com>
2025-12-31 12:25:00 +0530

    fix(unattended-install): make more things behave unattended

--- config.sh
+++ config.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/bash
+
+# Function to keep sudo credentials fresh
+keep_sudo_alive() {
+    sudo -v
+    while true; do
+        sudo -n true
+        sleep 50
+        kill -0 "$$" 2>/dev/null || exit
+    done 2>/dev/null &
+    SUDO_KEEPALIVE_PID=$!
+}
+
+# Function to stop the keep-alive process
+stop_sudo_keepalive() {
+    if [ -n "$SUDO_KEEPALIVE_PID" ]; then
+        kill "$SUDO_KEEPALIVE_PID" 2>/dev/null
+        wait "$SUDO_KEEPALIVE_PID" 2>/dev/null
+    fi
+}
+
+keep_sudo_alive
+
 BASEDIR=$(pwd)
 
 # Update and upgrade your base system
@@ -10,51 +32,51 @@ sudo apt install -y  git vim wget curl neovim
 # Run linking script for linking up dotfiles
 bash "$BASEDIR"/link.sh
 
-# Install Fira Code Nerd Font
-curl -OLs https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.tar.xz
-curl -OLs https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CommitMono.tar.xz
-curl -OLs https://github.com/ryanoasis/nerd-fonts/releases/latest/download/BitstreamVeraSansMono.tar.xz
+install_nerd_font() {
+    base_dir=$(pwd)
+    font_name="$1"
+    curl -OLs "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${font_name}.tar.xz"
+    mv "${base_dir}/${font_name}.tar.xz" /tmp
+    cd /tmp || echo "failed to go to /tmp" && return
 
-mv "$BASEDIR"/FiraCode.tar.xz /tmp
-mv "$BASEDIR"/CommitMono.tar.xz /tmp
-mv "$BASEDIR"/BitstreamVeraSansMono.tar.xz /tmp
+    tar xvf "${font_name}.tar.xz"
 
-cd /tmp || echo "failed to go to /tmp; tar may fail on extracting"
+    sudo mv ./*ttf /usr/share/fonts/truetype 2>/dev/null
+    sudo mv ./*otf /usr/share/fonts/opentype 2>/dev/null
+    rm "/tmp/${font_name}.tar.xz"
+}
 
-tar xvf FiraCode.tar.xz
-tar xvf CommitMono.tar.xz
-tar xvf BitstreamVeraSansMono.tar.xz
+if cd /tmp ; then
+    # Install Fira Code Nerd Font
+    install_nerd_font FiraCode
+    install_nerd_font CommitMono
+    install_nerd_font Inconsolata
+    install_nerd_font Hack
+    install_nerd_font JetBrainsMono
+    install_nerd_font NotoMono
+    install_nerd_font RobotoMono
+    install_nerd_font AdwaitaMono
 
-sudo mv ./*ttf /usr/share/fonts/truetype
-sudo mv ./*otf /usr/share/fonts/opentype
-
-sudo fc-cache -f -v
-
-rm /tmp/FiraCode.tar.xz
-rm /tmp/CommitMono.tar.xz
-rm /tmp/BitstreamVeraSansMono.tar.xz
+    sudo fc-cache -f -v
+else
+    echo "===== failed to go to /tmp for installing fonts, install them manually ====="
+fi
 
 cd "$BASEDIR" || echo "failed to go back to $BASEDIR after installing fonts in /tmp"
 
-# Install zsh, ohmyzsh, starship, zsh-autosuggestions, fast-syntax-highlighting
 sudo apt install -y zsh
-# Get Ohmyzsh, this also changes default shell to zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Install fast syntax highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting"
 
-# Install auto-suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
-## Install HomeBrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+## Install HomeBrew non-interactively
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 # Make sure brew is found in path during this install
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-# Get the list of installed packages
-# brew list --installed-on-request
 
 # Install from brewfile
 # Temporarily increase system's file descriptor limit so that brew passes while installing lots of packages
@@ -62,7 +84,7 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 BEFORE_ULIMIT_CHANGE=$(ulimit -n)
 ulimit -n 65535
 # xargs brew install < "${BASEDIR}/Brewfile"
-brew bundle "${BASEDIR}/Brewfile"
+brew bundle install --file="${BASEDIR}/Brewfile"
 ulimit -n "$BEFORE_ULIMIT_CHANGE"
 
 # Set bat themes https://github.com/catppuccin/bat
@@ -74,54 +96,42 @@ wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/
 bat cache --build
 
 git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
-bash "$HOME/.fzf/install"
+bash "$HOME"/.fzf/install --all
 # automatically checks and adds necessary additions to shell scripts, but we did that before, at this point it should be there
 
 # Get Micro as a text editor
-curl https://getmic.ro | bash
+curl -s https://getmic.ro | bash
 # move it to bin for systemwide
-sudo mv -i ./micro /usr/bin
+sudo mv -i ./micro /usr/local/bin
 
 # Setup micro, make a directory if not exists, and copy recursively settings and colorschemes
 cp -r "$BASEDIR/micro" "$HOME/.config"
 
 # Get NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-# brew install fnm
 # enable fnm for session
 eval "$(fnm env --use-on-cd --shell zsh)"
 LATEST_NODE_MAJOR=$(fnm ls-remote | tail -1 | tr -d v | cut -d. -f1)
 fnm install "$LATEST_NODE_MAJOR"
 
-# install global npm packages
-while read -r package; do npm i -g "$package"; done < "$BASEDIR/npm-global-packages.txt"
-
-# Install terminator
-# sudo apt install -y terminator
-
-# copy over terminator config
-# mkdir -p $HOME/.config/terminator
-# cp $BASEDIR/terminator.config $HOME/.config/terminator/config
-
 # Install wezterm
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
-sudo apt update
-sudo apt install wezterm
+sudo apt update -y && sudo apt install -y wezterm
 
-sudo apt install wl-clipboard  # wl-copy for wayland https://superuser.com/questions/1189467/how-to-copy-text-to-the-clipboard-when-using-wayland
+sudo apt -y install wl-clipboard  # wl-copy for wayland https://superuser.com/questions/1189467/how-to-copy-text-to-the-clipboard-when-using-wayland
 
 # Zathura for pdf
-sudo apt install zathura
+sudo apt install -y zathura
 
 ########################## OTHER SOFTWARE #########################
 
 ## Install snaps
 
 echo "Installing Snap and snaps - mailspring, notion, calendar, telegram, slack"
-sudo apt install snapd
+sudo apt install -y snapd
 sudo snap install mailspring notion-snap-reborn telegram-desktop slack notion-calendar-snap
 
 echo "Installing Flatpaks, RESTART NEEDED LATER TO MANUALLY INSTALL FLATPAKS"
@@ -135,16 +145,14 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 # ca.desrt.dconf-editor com.discordapp.Discord net.hovancik.Stretchly org.fedoraproject.MediaWriter
 
 # OBS Studio
-sudo apt install obs-studio
+sudo apt install -y obs-studio
 
-# Install Pandoc and Eisvogel
-
-sudo apt install pandoc
+# Install TexLive and Eisvogel
 
 read -r -p "Do you want to install texlive now? (y/n) " REPLY
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Installing texlive"
-  sudo apt install texlive
+    sudo apt install -y texlive
 else
     echo "Skipping texlive installation"
     echo "To install texlive later, run the following command:"
@@ -152,9 +160,10 @@ else
 fi
 
 # Eisvogel Latex template - https://github.com/Wandmalfarbe/pandoc-latex-template
-wget "https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex"
 mkdir -p "$HOME/.local/share/pandoc/templates"
-mv "$BASEDIR/eisvogel.tex" "$HOME/.local/share/pandoc/templates/eisvogel.tex"
+echo "======"
+echo "Install eisvogel.latex in $HOME/.local/share/pandoc/templates"
+echo "======"
 
 ## VSCODE https://code.visualstudio.com/
 # GOOGLE CHROME https://www.google.com/chrome/
@@ -162,13 +171,12 @@ mv "$BASEDIR/eisvogel.tex" "$HOME/.local/share/pandoc/templates/eisvogel.tex"
 
 # ---
 
-
 # Install other utilities
-
-sudo apt install rlwrap # wrap another program in readline
+sudo apt install -y rlwrap # wrap another program in readline
 
 # Rust
 command curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# shellcheck disable=SC1091
 [ -f "$HOME/.cargo/env" ] && source "$HOME/.cargo/env" # add to path for this script
 
 cargo install xcp http-server oxipng tinted-builder-rust xcp
@@ -182,5 +190,7 @@ export PATH=${GOPATH:=$HOME/go}/bin:$PATH # Add Go bin to path
 while read -r package; do go install "$package"; done < "$BASEDIR/go-binary-packages.txt"
 # golang.org/x/tools/cmd/{gopls,staticcheck} should be prompted to be installed from the go vscode extension anyways
 
-# Node Global Packages
+# Install global npm packages
 while read -r package; do npm i -g "$package"; done < "$BASEDIR/npm-global-packages.txt"
+
+stop_sudo_keepalive
--- link.sh
+++ link.sh
@@ -2,9 +2,17 @@
 
 BASEDIR=$(pwd)
 
-DOTFILES=(.bash_profile .bashrc .gitconfig .gitignore .inputrc .npmrc .zshrc .bash bin .czrc .zshenv .config/starship.toml .config/ghostty .config/zathura .config/wezterm .config/atuin/config.toml .config/tmux/tmux.conf .hushlogin)
+# link them as files or directory directly
+DOTFILES=(.bash_profile .bashrc .gitconfig .gitignore .inputrc .npmrc .zshrc .bash bin .czrc .zshenv .config/starship.toml .config/ghostty .config/zathura .config/wezterm .hushlogin .config/atuin/config.toml .config/tmux/tmux.conf)
+
+# create parent dir before linking the above files
+DOTFILES_PRE_DIR=(.config/atuin .config/tmux)
 
 mkdir -p "$HOME"/.config
+for d in "${DOTFILES_PRE_DIR[@]}"; do
+    echo "Creating dir $d"
+    mkdir -p "$d"
+done
 
 for dot in "${DOTFILES[@]}"; do
 	echo "Removing $HOME/$dot"
